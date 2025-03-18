2a5df4da119ae6d878c851efc35199b3a4e071e9
Tribak-Ayoub <tribak.elyedri.ayoub@gmail.com>
2025-03-18 18:09:59 +0000

    setup the vue and test the route

---
+++ App/resources/js/router.js
@@ -0,0 +1,12 @@
+import {createRouter, createWebHistory} from 'vue-router';
+
+const routes = [
+    { path: '/', component: () => import('../../modules/PkgBlog/Resources/js/pages/admin/article/index.vue') },
+]
+
+const router = createRouter({
+    history: createWebHistory(),
+    routes,
+})
+
+export default router;
