8f37dd8abf2c46dcc8a7bd377bac42ad7445e72d
Tribak-Ayoub <tribak.elyedri.ayoub@gmail.com>
2025-03-18 16:15:59 +0000

    register the blog and core provider in the main provider

--- App/bootstrap/providers.php
+++ App/bootstrap/providers.php
@@ -2,4 +2,6 @@
 
 return [
     App\Providers\AppServiceProvider::class,
+    Modules\Core\App\Providers\CoreServiceProvider::class,
+    Modules\PkgBlog\App\Providers\PkgBlogServiceProvider::class,
 ];
---
+++ App/modules/PkgBlog/App/Providers/PkgBlogServiceProvider.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Modules\PkgBlog\App\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+class PkgBlogServiceProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     */
+    public function register(): void
+    {
+        //
+    }
+
+    /**
+     * Bootstrap services.
+     */
+    public function boot(): void
+    {
+        //
+    }
+}
