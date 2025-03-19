f744c7ba9c182d310ff8b951449dc30db029468a
Tribak-Ayoub <tribak.elyedri.ayoub@gmail.com>
2025-03-19 21:52:24 +0000

    create the article service

--- App/modules/PkgBlog/App/Controllers/ArticleController.php
+++ App/modules/PkgBlog/App/Controllers/ArticleController.php
@@ -7,5 +7,38 @@ use Modules\Core\App\Controllers\BaseController;
 
 class ArticleController extends BaseController
 {
-    //
+    public function index()
+    {
+        return view('PkgBlog::index');
+    }
+
+    public function show($id)
+    {
+        return view('PkgBlog::show', compact('id'));
+    }
+
+    public function create()
+    {
+        return view('PkgBlog::create');
+    }
+
+    public function store(Request $request)
+    {
+        return redirect()->route('articles.index');
+    }
+
+    public function edit($id)
+    {
+        return view('PkgBlog::edit', compact('id'));
+    }
+
+    public function update(Request $request, $id)
+    {
+        return redirect()->route('articles.index');
+    }
+
+    public function destroy($id)
+    {
+        return redirect()->route('articles.index');
+    }
 }
