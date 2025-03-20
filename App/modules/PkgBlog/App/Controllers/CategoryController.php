815c335711f637dc30dbcd6db0b616ea5dca3c90
Tribak-Ayoub <tribak.elyedri.ayoub@gmail.com>
2025-03-20 09:54:35 +0000

    category controller

--- App/modules/PkgBlog/App/Controllers/CategoryController.php
+++ App/modules/PkgBlog/App/Controllers/CategoryController.php
@@ -5,15 +5,25 @@ namespace Modules\PkgBlog\App\Controllers;
 use Illuminate\Http\Request;
 use Modules\Core\App\Controllers\BaseController;
 use Modules\PkgBlog\App\Models\Category;
+use Modules\PkgBlog\App\Services\CategoryService;
 
 class CategoryController extends BaseController
 {
+    protected $categoryService;
+
+    public function __construct(CategoryService $categoryService)
+    {
+        $this->categoryService = $categoryService;
+    }
+
     /**
      * Display a listing of the resource.
      */
     public function index()
     {
-        //
+        $categories = $this->categoryService->paginate();
+
+        return response()->json($categories);
     }
 
     /**
@@ -29,7 +39,13 @@ class CategoryController extends BaseController
      */
     public function store(Request $request)
     {
-        //
+        $data = $request->validate([
+            'name' => 'required|string',
+        ]);
+
+        $category = $this->categoryService->createCategory($data);
+
+        return response()->json($category);
     }
 
     /**
@@ -45,7 +61,9 @@ class CategoryController extends BaseController
      */
     public function edit(Category $category)
     {
-        //
+        $category = $this->categoryService->getCategoryById($category->id);
+
+        return response()->json($category);
     }
 
     /**
@@ -53,7 +71,13 @@ class CategoryController extends BaseController
      */
     public function update(Request $request, Category $category)
     {
-        //
+        $data = $request->validate([
+            'name' => 'required|string',
+        ]);
+
+        $category = $this->categoryService->updateCategory($category, $data);
+
+        return response()->json($category);
     }
 
     /**
@@ -61,6 +85,8 @@ class CategoryController extends BaseController
      */
     public function destroy(Category $category)
     {
-        //
+        $this->categoryService->deleteCategory($category);
+
+        return response()->json(null, 204);
     }
 }
