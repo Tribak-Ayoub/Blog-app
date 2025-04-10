cd35bee5aced674c1b3f9be8a10cc3b4cbc7d677
Tribak-Ayoub <tribak.elyedri.ayoub@gmail.com>
2025-04-10 13:25:49 +0100

    create comment service

---
+++ App/modules/PkgBlog/App/Services/CommentService.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace Modules\PkgBlog\App\Services;
+
+use Illuminate\Support\Facades\Auth;
+use Modules\PkgBlog\App\Models\Comment;
+
+class CommentService
+{
+    public function paginate($search, $perPage = 10)
+    {
+        $query = Comment::latest()->with(['user', 'article']);
+
+        // Apply search filter
+        if (!empty($search)) {
+            $query->where(function ($query) use ($search) {
+                $query->where('title', 'like', '%' . $search['search'] . '%');
+            });
+        }
+
+        return $query->paginate($perPage);
+    }
+
+    public function getCommentById($id)
+    {
+        return Comment::findOrFail($id);
+    }
+
+    public function createComment(array $data)
+    {
+        return Comment::create([
+            'content' => $data['content'],
+            'user_id' => Auth::id(),
+            'article_id' => $data['article_id'],
+        ]);
+    }
+
+    public function updateComment(Comment $comment, array $data)
+    {
+        $comment->update([
+            'content' => $data['content'],
+            'user_id' => Auth::id(),
+            'article_id' => $data['article_id'],
+        ]);
+
+        return $comment;
+    }
+
+    public function deleteComment(Comment $comment)
+    {
+        return $comment->delete();
+    }
+
+}
