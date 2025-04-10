fc8b51e94fa1ca44c3e5475b04eb07eb1763cfc4
Tribak-Ayoub <tribak.elyedri.ayoub@gmail.com>
2025-04-10 14:41:22 +0100

    craete comment controller and class request

--- App/modules/PkgBlog/App/Controllers/CommentController.php
+++ App/modules/PkgBlog/App/Controllers/CommentController.php
@@ -2,18 +2,29 @@
 
 namespace Modules\PkgBlog\App\Controllers;
 
-use Illuminate\Http\Request;
+use Illuminate\Http\Response;
 use Modules\Core\App\Controllers\BaseController;
 use Modules\PkgBlog\App\Models\Comment;
+use Modules\PkgBlog\App\Requests\CommentRequest;
+use Modules\PkgBlog\App\Services\CommentService;
 
 class CommentController extends BaseController
 {
+    protected $commentService;
+
+    public function __construct(CommentService $commentService)
+    {
+        $this->commentService = $commentService;
+    }
     /**
      * Display a listing of the resource.
      */
     public function index()
     {
-        //
+        $search = request()->search;
+        $comments = $this->commentService->paginate($search);
+
+        return response()->json($comments);
     }
 
     /**
@@ -27,40 +38,62 @@ class CommentController extends BaseController
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
+    public function store(CommentRequest $request)
     {
-        //
+        $data = $request->validated();
+        $comment = $this->commentService->createComment($data);
+
+        return response()->json([
+            'message' => "The comment has been created",
+            'comment' => $comment,
+        ], Response::HTTP_CREATED);
     }
 
     /**
      * Display the specified resource.
      */
-    public function show(Comment $comment)
+    public function show($id)
     {
-        //
+        $comment = $this->commentService->getCommentById($id);
+
+        return response()->json($comment);
     }
 
     /**
      * Show the form for editing the specified resource.
      */
-    public function edit(Comment $comment)
+    public function edit($id)
     {
-        //
+        $comment = Comment::findOrFail($id);
+
+        return response()->json($comment);
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, Comment $comment)
+    public function update(CommentRequest $request, $id)
     {
-        //
+        $comment = Comment::findOrFail($id);
+        $validated = $request->validated();
+        $this->commentService->updateComment($comment, $validated);
+
+        return response()->json([
+            'message' => "The comment has been updated",
+            'comment' => $comment->fresh(),
+        ]);
     }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(Comment $comment)
+    public function destroy($id)
     {
-        //
+        $comment = Comment::findOrFail($id);
+        $this->commentService->deleteComment($comment);
+
+        return response()->json([
+            'message' => "The comment has been deleted"
+        ], Response::HTTP_NO_CONTENT);
     }
 }
