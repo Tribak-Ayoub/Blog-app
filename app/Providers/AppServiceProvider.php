fd7aec50e738ca3c152a073246c663cc7762cd95
Tribak-Ayoub <tribak.elyedri.ayoub@gmail.com>
2025-06-05 16:53:56 +0000

    update the config

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -22,10 +22,10 @@ class AppServiceProvider extends ServiceProvider
     /**
      * Bootstrap any application services.
      */
-    public function boot(): void
+    public function boot()
     {
-        if (app()->environment('production')) {
-            URL::forceScheme('https');
+        if (!file_exists(database_path('database.sqlite'))) {
+            \Log::error('SQLite database file is missing!');
         }
     }
 }
